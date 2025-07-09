e2bfb81d583521ee7f4f969dd57f3242be4c8354
VineeshHS <vineeshhs367@gmail.com>
2025-07-09 16:29:07 +0530

    Initial Commit

---
+++ ArtGalleryExplorerApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtGalleryExplorerApplication
+//
+//  Created by ViNEESH HS on 09/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ArtGalleryExplorerApplicationTests/ArtGalleryExplorerApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryExplorerApplicationTests.swift
+//  ArtGalleryExplorerApplicationTests
+//
+//  Created by ViNEESH HS on 09/07/25.
+//
+
+import Testing
+@testable import ArtGalleryExplorerApplication
+
+struct ArtGalleryExplorerApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
